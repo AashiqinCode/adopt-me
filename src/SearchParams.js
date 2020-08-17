8d834358225112554b04e624af9e636fedaac638
AashiqinCode <aashiqahmed10101@gmail.com>
2020-08-17 21:07:57 +0530

    creating a custom hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   // console.log(ANIMALS);
 
   const [location, setLocation] = useState("Delhi ,IN");
-  const [animal, setAnimal] = useState("Dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  // const [animal, setAnimal] = useState("Dog");
+  // const [breed, setBreed] = useState("");
+
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "Dog", breeds);
   return (
     <div className="search-params">
       <h2>{location}</h2>
@@ -22,40 +26,9 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        {/* Breed */}
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed} //This value is sent to the server
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disabled={!breeds.length}
-          >
-            <option>All</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
       </form>
     </div>
   );
